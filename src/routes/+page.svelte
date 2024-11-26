0b8f1322b8a7955db9a3d89581eb514b8a610d69
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-11-26 13:17:20 +1300

    Add function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
+<script lang="ts">
+  let wrikeId = $state('');
+
+  function updateWrikeId(e) {
+    wrikeId = e.target.value;
+  }
+
+  function openWrike() {
+    const url = `https://www.wrike.com/open.htm?id=${wrikeId}`;
+    window.open(url);
+  }
+</script>
+
 <div class="flex h-screen">
   <div class="m-auto flex gap-x-4">
     <label for="wrike-id" class="sr-only">Wrike Ticket ID</label>
@@ -6,12 +19,15 @@
       name="wrikeid"
       type="text"
       required
-      class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-indigo-500 focus:ring-1 focus:ring-inset focus:ring-indigo-500 sm:text-sm/6"
+      class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-indigo shadow-sm ring-1 ring-inset ring-indigo-500 focus:ring-1 focus:ring-inset focus:ring-indigo-500 sm:text-sm/6"
       placeholder="Wrike Ticket ID"
+      onkeyupcapture={updateWrikeId}
+
     >
     <button
       type="submit"
       class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+      onclick={openWrike}
     >
       Go
     </button>
