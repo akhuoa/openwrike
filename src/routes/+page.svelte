58bb312e46a316d33c7e26091e5372a83b5d3f2e
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-11-26 13:03:23 +1300

    Update UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-screen">
+  <div class="m-auto flex gap-x-4">
+    <label for="wrike-id" class="sr-only">Wrike Ticket ID</label>
+    <input
+      id="wrike-id"
+      name="wrikeid"
+      type="text"
+      required
+      class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-indigo-500 focus:ring-1 focus:ring-inset focus:ring-indigo-500 sm:text-sm/6"
+      placeholder="Wrike Ticket ID"
+    >
+    <button
+      type="submit"
+      class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+    >
+      Go
+    </button>
+  </div>
+</div>
